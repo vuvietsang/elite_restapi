2cbe610234507291bc783e2091d9796b066047ee
sang <vuvietsang10a9@gmail.com>
2022-04-13 19:52:44 +0700

    resolve secretkey null

--- src/main/java/com/example/elite/jwt/TokenVerifier.java
+++ src/main/java/com/example/elite/jwt/TokenVerifier.java
@@ -1,15 +1,20 @@
 package com.example.elite.jwt;
-
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.crypto.SecretKey;
@@ -22,13 +27,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class TokenVerifier extends OncePerRequestFilter {
-    private JwtConfig jwtConfig;
-    private SecretKey secretKey;
-
+    private final JwtConfig jwtConfig;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String authorizationHeader = request.getHeader("Authorization");
@@ -38,7 +39,7 @@ public class TokenVerifier extends OncePerRequestFilter {
         }
         try {
             String token = authorizationHeader.replace(jwtConfig.getTokenPrefix(),"");
-            Jws<Claims> claimsJws = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            Jws<Claims> claimsJws = Jwts.parserBuilder().setSigningKey(Keys.hmacShaKeyFor(jwtConfig.getSecretKey().getBytes())).build().parseClaimsJws(token);
             Claims body = claimsJws.getBody();
             String username = body.getSubject();
             var authorities = (List<Map<String, String>>)body.get("authorities");
