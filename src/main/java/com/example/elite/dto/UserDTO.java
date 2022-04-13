3b83f8a8acc7aec5efd6b0dabdc18989699338b3
sang <vuvietsang10a9@gmail.com>
2022-04-13 21:10:10 +0700

    add User DTO

---
+++ src/main/java/com/example/elite/dto/UserDTO.java
@@ -0,0 +1,19 @@
+package com.example.elite.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserDTO {
+    private int userId;
+    private String fullName;
+    private String username;
+    private String email;
+    private String roleName;
+    private String phone;
+}
